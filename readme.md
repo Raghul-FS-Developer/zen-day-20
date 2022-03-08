974da5041a74cadc43cd6019633f011982cded75
Raghul <88141940+princeraghul@users.noreply.github.com>
2022-03-08 17:01:46 +0530

    first commit

--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
-## Lovie
+## Newsie
 
 #### About
 
-- Love calculator using love calculator public api
+- News app using public API
 - Due to CORS error I signed up in rapidapi and got the api key's
 
 #### What it does?
 
-- On entering the name of bride and groom, it displays percentage and suggestion whether to move with the pair or not
+- On searching for topics it displays relevant articles and error message when not found any relevant articles.
+- Searches are stored locally in local storage for serving the news fastly for repeated search.
+- Also pills/chips of recent searches are displayed on top, when user clicks on them, articles are served from local storage.
--- script.js
+++ script.js
@@ -1,55 +1,114 @@
-let resultCard = document.createElement("DIV");
-resultCard.classList.add("card", "text-white", "mb-3");
 
-let resultPercent = document.createElement("DIV");
-resultPercent.id = "percent";
-resultPercent.className = "card-header";
 
-let resultMessage = document.createElement("DIV");
-resultMessage.id = "message";
-resultMessage.className = "card-body";
+function loading(status) {
+  const searching = document.getElementById('loading');
+  if (status) {
+    searching.className = "is__loading";
+  } else {
+    searching.className = "not__loading";
+  }
+}
 
-const brideName = document.getElementById("bname");
-const groomName = document.getElementById("gname");
-const error = document.getElementById("error");
-
-function setError(msg) {
+function setErrorMessage(msg) {
+  const error = document.getElementById('error');
   error.innerText = msg;
 }
+/*
+  A Function that is called on body load gets the keys(searched words) from
+  local storage and iterate to render pills/chips style recent searches on screen.
+*/ 
+async function recentSearches() {
+  await loading(true);
+  let history = { ...localStorage };
+  history = Object.keys(history);
+  if (history.length) {
+    let recentlySearched = "";
+    for (let i of history) {
+      recentlySearched += `<button  class="btn btn-light btn-sm chip"
+       onclick="renderRecentlySearchedNews('${i}');">${i}</button>`;
+    }
 
-function renderResult(result) {
-  document.getElementById("result").innerHTML = "";
-  if (result.percentage >= 51) {
-    resultCard.classList.remove("bg-primary");
-    resultCard.classList.add("bg-success");
-  } else if (result.percentage <= 50) {
-    resultCard.classList.remove("bg-success");
-    resultCard.classList.add("bg-primary");
+    document.getElementById(
+      "recent"
+    ).innerHTML = `Recently searched: ${recentlySearched}`;
   }
-  resultPercent.innerText = `${result.percentage}%`;
-  resultMessage.innerHTML = `<h5 class="card-title">${result.result}</h5>`;
-  resultCard.append(resultPercent, resultMessage);
-  document.getElementById("result").appendChild(resultCard);
-}
+  await loading(false);
+};
 
-function calculate() {
-  if (brideName.value === "" || groomName.value === "") {
-    setError("Name field cannot be empty!!!");
+
+const getReadmore = (article) => {
+  let lengthOfSummary = Math.floor(article.summary.length / 4);
+  return `${article.summary.slice(0, lengthOfSummary)} <a href="${
+    article.link
+  }" target="_blank">read more</a>`;
+};
+
+async function renderRecentlySearchedNews(){
+  await loading(true);
+  renderNews(JSON.parse(localStorage.getItem(q)));
+  await loading(false);
+};
+
+const renderNews = (response) => {
+  const newsParent = document.getElementById("news");
+  let news = "";
+  let articles = response["articles"];
+  for (let i = 0; i < articles.length; i++) {
+    news += `
+    <div class="col news__card">
+    <div class="card h-100">
+      <img src="${articles[i].media}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${articles[i].title}</h5>
+        <p class="card-text">${getReadmore(articles[i])}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Published: ${
+          articles[i].published_date
+        }</small>
+      </div>
+    </div>
+  </div>
+    `;
+  }
+  newsParent.innerHTML = news;
+};
+
+async function search() {
+  await loading(true);
+  const q = document.getElementById("q");
+  if (q.value === "") {
+   setErrorMessage(`Search cannot be empty!!!`);
   } else {
-    fetch(
-      `https://love-calculator.p.rapidapi.com/getPercentage?sname=${bname.value.trim()}&fname=${gname.value.trim()}`,
-      {
+    if (localStorage.getItem(q.value) === null) {
+      const uri = `https://free-news.p.rapidapi.com/v1/search?q=${q.value}&lang=en`;
+      fetch(uri, {
         method: "GET",
         headers: {
-          "x-rapidapi-host": "love-calculator.p.rapidapi.com",
-          "x-rapidapi-key": "bca5fef741msha20a0b8b597d93dp14afdbjsnc0eed58ec116"
+          "x-rapidapi-host": "free-news.p.rapidapi.com",
+          "x-rapidapi-key": "880b0ba246mshe7a6c0e5491f12ap1347e9jsn1060b255d49c"
         }
-      }
-    )
-      .then((response) => response.json())
-      .then((result) => renderResult(result))
-      .catch((err) => {
-        console.error(err);
-      });
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.status === "ok") {
+            localStorage.setItem(q.value, JSON.stringify(response));
+            recentSearches();
+            renderNews(response);
+          } else {
+            setErrorMessage(response.status);
+          }
+       
+        })
+        .catch((err) => {
+          setErrorMessage(response.status);
+          console.error(err);
+        });
+    } else {
+      // console.log("From local storage");
+      renderNews(JSON.parse(localStorage.getItem(q.value)));
+
+    }
   }
+  await loading(false);
 }
